7db1f205ca74c0272a730f9bba1b08ff236f37fb
Eugene Vorontsov <eugene.vorontsov@gmail.com>
2015-09-20 20:53:13 -0400

    Generalized compare_meshes code to be able to take meshes or images as inputs and handle the different types automatically. Results can now be output to a file.

--- src/compare_meshes.cpp
+++ src/compare_meshes.cpp
@@ -1,3 +1,8 @@
+/* Author: Eugene Vorontsov
+ *
+ * Load any two files of type image mask (target value 1, else 0) or vtk/vtp mesh and compare the two surfaces using MeshValmet.
+ * Code for converting itk to vtk meshes taken from Arnaud Gelas <arnaud_gelas@hms.harvard.edu> from github (https://github.com/arnaudgelas/itkQuadEdgeMeshProcessing) */
+
 #include <unistd.h>
 #include <iostream>
 
@@ -7,6 +12,7 @@
 #include <vtkPolyDataWriter.h>
 
 #include <itkImageFileReader.h>
+#include <itkCastImageFilter.h>
 #include <itkBinaryMask3DMeshSource.h>
 #include <itkSimplexMeshToTriangleMeshFilter.h>
 
@@ -14,145 +20,228 @@
 #include "VTK_to_MeshValmet.h"
 #include "CompareMeshes.h"
 
-template < typename TPixel, unsigned int VImageDimension >
-void RunCompareMeshes(std::string filename_image, std::string filename_mesh);
+enum file_type {IMAGE, VTK, VTP, UNKNOWN};
 
-int main(int argc, char** argv)
+/* Identify file type: ITK image, vtk/vtp mesh, unkown. */
+file_type identify_file_type(std::string filename)
 {
-  if( argc != 3 )
+  // Check for image (by header)
+  itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImageIO( filename.c_str(), itk::ImageIOFactory::ReadMode );
+  if( imageIO.IsNotNull() )
   {
-    std::cerr << "Usage: " << std::endl;
-    std::cerr << argv[0] << " <mesh filename> <ground truth image filename>" << std::endl;
-    return 1;
+    return IMAGE;
   }
   
-  // Get mesh filename
-  std::string filename_mesh = argv[1];
+  // Check for mesh (by file extension)
+  std::string::size_type idx = filename.rfind('.');
+  if(idx != std::string::npos)
+  {
+    std::string extension = filename.substr(idx+1);
+    if(extension.compare("vtk")==0)
+    {
+      return VTK;
+    }
+    if(extension.compare("vtp")==0)
+    {
+      return VTP;
+    }
+  }
   
-  // Identify image type and load it
-  std::string filename_image = argv[2];
-  
-  itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImageIO( filename_image.c_str(), itk::ImageIOFactory::ReadMode );
-  imageIO->SetFileName(filename_image);
+  // Unknown file type
+  return UNKNOWN;
+}
+
+
+
+
+/* Load an image and return the image with float type pixels.
+   Supporting DICOM, MetaImage (see http://www.itk.org/Wiki/ITK/File_Formats) */
+template < typename TPixel, typename TCastPixel >
+typename itk::Image<TCastPixel, 3>::Pointer load_and_cast_image3D_subroutine(std::string filename);
+template < typename TCastPixel >
+typename itk::Image<TCastPixel, 3>::Pointer load_and_cast_image3D(std::string filename)
+{
+  // Identify image type
+  itk::ImageIOBase::Pointer imageIO = itk::ImageIOFactory::CreateImageIO( filename.c_str(), itk::ImageIOFactory::ReadMode );
+  imageIO->SetFileName(filename);
   imageIO->ReadImageInformation();
   itk::ImageIOBase::IOPixelType PixelType = imageIO->GetPixelType();
   itk::ImageIOBase::IOComponentType PixelValueType = imageIO->GetComponentType();
+  if( imageIO->GetNumberOfDimensions() != 3 )
+  {
+    std::cerr << "ERROR: There are fewer or more than 3 dimensions in the image file: " << filename << std::endl;
+    throw 2;
+  }
   
-  // Supporting DICOM, MetaImage (see http://www.itk.org/Wiki/ITK/File_Formats):
+  // Load and cast the image
   if( PixelType == itk::ImageIOBase::SCALAR )
   {
     switch( PixelValueType )
     {
       case itk::ImageIOBase::FLOAT  :
-        RunCompareMeshes<float, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<float, TCastPixel>(filename);
       case itk::ImageIOBase::DOUBLE  :
-        RunCompareMeshes<double, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<double, TCastPixel>(filename);
       case itk::ImageIOBase::CHAR  :
-        RunCompareMeshes<char, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<char, TCastPixel>(filename);
       case itk::ImageIOBase::UCHAR  :
-        RunCompareMeshes<unsigned char, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<unsigned char, TCastPixel>(filename);
       case itk::ImageIOBase::SHORT  :
-        RunCompareMeshes<short, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<short, TCastPixel>(filename);
       case itk::ImageIOBase::USHORT  :
-        RunCompareMeshes<unsigned short, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<unsigned short, TCastPixel>(filename);
       case itk::ImageIOBase::LONG  :
-        RunCompareMeshes<long, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<long, TCastPixel>(filename);
       case itk::ImageIOBase::ULONG  :
-        RunCompareMeshes<unsigned long, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<unsigned long, TCastPixel>(filename);
       case itk::ImageIOBase::INT  :
-        RunCompareMeshes<int, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<int, TCastPixel>(filename);
       case itk::ImageIOBase::UINT  :
-        RunCompareMeshes<unsigned int, 3>(filename_image, filename_mesh);
-        break;
-        
+        return load_and_cast_image3D_subroutine<unsigned int, TCastPixel>(filename);
       default:
         std::cerr << "Unsupported pixel value type: " << imageIO->GetComponentTypeAsString( PixelValueType )
         << std::endl;
-        return 2;
+        throw 2;
     }
   }
   else
   {
     std::cerr << "File format not supported. Only DICOM and MetaImage files are supported.\n";
     std::cerr << "Unsupported pixel type: " << imageIO->GetPixelTypeAsString( PixelType ) << std::endl;
+    throw 2;
   }
-  
-  return 0;
 }
 
-template < typename TPixel, unsigned int VImageDimension >
-void RunCompareMeshes(std::string filename_image, std::string filename_mesh)
+template < typename TPixel, typename TCastPixel >
+typename itk::Image<TCastPixel, 3>::Pointer load_and_cast_image3D_subroutine(std::string filename)
 {
-  // Load image
-  typedef itk::Image< TPixel, VImageDimension > ImageType;
-  typedef itk::ImageFileReader<ImageType> ReaderType;
+  // Load the image
+  typedef itk::Image< TPixel, 3 > LoadImageType;
+  typedef itk::ImageFileReader<LoadImageType> ReaderType;
   typename ReaderType::Pointer reader = ReaderType::New();
-  reader->SetFileName( filename_image );
+  reader->SetFileName( filename );
   reader->Update();
   
-  // Convert the image into a mesh
-  typedef itk::DefaultDynamicMeshTraits<double, 3, 3,double,double> TriangleMeshTraits;
-  typedef itk::Mesh<double,3, TriangleMeshTraits> TriangleMeshType;
-  typedef itk::BinaryMask3DMeshSource< ImageType, TriangleMeshType > MaskToMeshType;
-  typename MaskToMeshType::Pointer mask2mesh = MaskToMeshType::New();
-  mask2mesh->SetInput( reader->GetOutput() );
-  mask2mesh->Update();
-  if( mask2mesh->GetOutput()->GetNumberOfPoints()==0 )
-  {
-    std::cerr << "Error: No points in the mesh." << std::endl;
-    return;
-  }
-  itkMeshTovtkPolyData itk2vtk;
-  itk2vtk.SetInput( mask2mesh->GetOutput() );
-  vtkSmartPointer<vtkPolyData> mesh1 = itk2vtk.GetOutput();
+  // Cast the image to an internal type
+  typedef itk::Image< TCastPixel, 3 > CastImageType;
+  typedef itk::CastImageFilter< LoadImageType, CastImageType > CastFilterType;
+  typename CastFilterType::Pointer cast_filter = CastFilterType::New();
+  cast_filter->SetInput( reader->GetOutput() );
+  cast_filter->Update();
   
-  // Read mesh2 from file
-//   vtkSmartPointer<vtkGenericDataObjectReader> reader1 = vtkSmartPointer<vtkGenericDataObjectReader>::New();
-  vtkSmartPointer<vtkXMLPolyDataReader> reader1 = vtkSmartPointer<vtkXMLPolyDataReader>::New();
-  reader1->SetFileName( filename_mesh.c_str() );
-  reader1->Update();
-//   vtkSmartPointer<vtkPolyData> mesh2 = reader1->GetPolyDataOutput();
-  vtkSmartPointer<vtkPolyData> mesh2 = reader1->GetOutput();
+  return cast_filter->GetOutput();
+}
+
+
+/* Load a file and return the contents as a mesh. If it's an mask, convert it to a mesh. */
+vtkSmartPointer<vtkPolyData> load_file_as_mesh(std::string filename, file_type type)
+{
+  switch( type )
+  {
+    case IMAGE :
+    {
+      // Load and cast the image to an internal type
+      typedef itk::Image< float, 3 > InternalImageType;
+      InternalImageType::Pointer image = load_and_cast_image3D<float>(filename);
+      
+      // Convert the image into a mesh
+      typedef itk::DefaultDynamicMeshTraits<double, 3, 3,double,double> TriangleMeshTraits;
+      typedef itk::Mesh<double,3, TriangleMeshTraits> TriangleMeshType;
+      typedef itk::BinaryMask3DMeshSource< InternalImageType, TriangleMeshType > MaskToMeshType;
+      typename MaskToMeshType::Pointer mask2mesh = MaskToMeshType::New();
+      mask2mesh->SetInput( image );
+      mask2mesh->Update();
+      if( mask2mesh->GetOutput()->GetNumberOfPoints()==0 )
+      {
+        std::cerr << "Error: No points in the mesh." << std::endl;
+        throw 2;
+      }
+      itkMeshTovtkPolyData itk2vtk;
+      itk2vtk.SetInput( mask2mesh->GetOutput() );
+      return itk2vtk.GetOutput();
+    } 
+    case VTK :
+    {
+      vtkSmartPointer<vtkGenericDataObjectReader> reader = vtkSmartPointer<vtkGenericDataObjectReader>::New();
+      reader->SetFileName( filename.c_str() );
+      reader->Update();
+      return reader->GetPolyDataOutput();
+    }
+    case VTP :
+    {
+      vtkSmartPointer<vtkXMLPolyDataReader> reader = vtkSmartPointer<vtkXMLPolyDataReader>::New();
+      reader->SetFileName( filename.c_str() );
+      reader->Update();
+      return reader->GetOutput();
+    }
+    case UNKNOWN :
+    {
+      std::cerr << "ERROR: Cannot load file; unknown file type for file: " << filename << std::endl;
+      throw 2;
+    }
+  }
+}
+
+
+int main(int argc, char** argv)
+{
+  if( argc!=3 and argc!=4 )
+  {
+    std::cerr << "Usage: " << std::endl;
+    std::cerr << argv[0] << " <mesh/image filename> <ground truth mesh/image filename> [<results file>]" << std::endl;
+    return 1;
+  }
+  
+  // Identify file types and load as meshes accordingly
+  file_type type1 = identify_file_type(argv[1]);
+  if( type1==UNKNOWN )
+  {
+    std::cerr << "Unknown file type for file: " << argv[1] << std::endl;
+    return 1;
+  }
+  file_type type2 = identify_file_type(argv[2]);
+  if( type2==UNKNOWN )
+  {
+    std::cerr << "Unknown file type for file: " << argv[2] << std::endl;
+    return 1;
+  }
+  vtkSmartPointer<vtkPolyData> mesh1 = load_file_as_mesh(argv[1], type1);
+  vtkSmartPointer<vtkPolyData> mesh2 = load_file_as_mesh(argv[2], type2);
   
   // Compare meshes using MeshValmet
   CompareMeshes* cm = new CompareMeshes();
   CompareMeshes::mesh_differences diff = cm->GetMeshDifferences( VTK_to_MeshValmet(mesh1), VTK_to_MeshValmet(mesh2) );
   
-//   // Output results
-//   std::cout << "MIN DIST: " << diff.min_dist << std::endl;
-//   std::cout << "MAX DIST: " << diff.max_dist << std::endl;
-//   std::cout << "MIN ABS DIST: " << diff.abs_min_dist << std::endl;
-//   std::cout << "MAX ABS DIST: " << diff.abs_max_dist << std::endl;
-//   std::cout << "MEAN DIST: " << diff.mean_dist << std::endl;
-//   std::cout << "MEAN ABS DIST: " << diff.abs_mean_dist << std::endl;
-//   std::cout << "RMS DIST: " << diff.rms_dist << std::endl;
-//   std::cout << "VOLUME OVERLAP: " << diff.volume_overlap << std::endl;
-//   std::cout << "INTERSECTION/UNION: " << diff.int_union_ratio << std::endl;
+  // Output results to stdout
+  std::cout << "MIN DIST: " << diff.min_dist << std::endl;
+  std::cout << "MAX DIST: " << diff.max_dist << std::endl;
+  std::cout << "MIN ABS DIST: " << diff.abs_min_dist << std::endl;
+  std::cout << "MAX ABS DIST: " << diff.abs_max_dist << std::endl;
+  std::cout << "MEAN DIST: " << diff.mean_dist << std::endl;
+  std::cout << "MEAN ABS DIST: " << diff.abs_mean_dist << std::endl;
+  std::cout << "RMS DIST: " << diff.rms_dist << std::endl;
+  std::cout << "VOLUME OVERLAP: " << diff.volume_overlap << std::endl;
+  std::cout << "INTERSECTION/UNION: " << diff.int_union_ratio << std::endl;
   
-  std::string results_filename = "results.txt";
-  FILE* results_file = fopen(results_filename.c_str(), "aw");
-  fprintf(results_file, "%s %s %s %s %s %s %s %s %s\n", "min_dist", "max_dist", "min_abs_dist", "max_abs_dist",
-          "mean_dist", "mean_abs_dist", "rms_dist", "volume_overlap", "int_over_union");
-  fprintf(results_file, "%f %f %f %f %f %f %f %f %f\n", diff.min_dist, diff.max_dist,
-          diff.abs_min_dist, diff.abs_max_dist, diff.mean_dist, diff.abs_mean_dist,
-          diff.rms_dist, diff.volume_overlap, diff.int_union_ratio);
-  fclose(results_file);
+  // Output results to a file (compact)
+  if( argc==4 )
+  {
+    try
+    {
+      FILE* results_file = fopen(argv[3], "aw");
+      fprintf(results_file, "%s %s %s %s %s %s %s %s %s\n", "min_dist", "max_dist", "min_abs_dist", "max_abs_dist",
+              "mean_dist", "mean_abs_dist", "rms_dist", "volume_overlap", "int_over_union");
+      fprintf(results_file, "%f %f %f %f %f %f %f %f %f\n", diff.min_dist, diff.max_dist,
+              diff.abs_min_dist, diff.abs_max_dist, diff.mean_dist, diff.abs_mean_dist,
+              diff.rms_dist, diff.volume_overlap, diff.int_union_ratio);
+      fclose(results_file);
+    }
+    catch(...)
+    {
+      std::cerr << "ERROR opening file to write results. Filename requested: " << argv[3] << std::endl;
+      return 2;
+    }
+  }
+  
+  return 0;
 }
